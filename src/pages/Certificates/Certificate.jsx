546d5b217e24ef42ffcdb4f044c1363b952b5eaf
Spring -Git <vaahne@yahoo.com>
2025-09-09 21:12:00 -0600

    added certificates

--- src/pages/Certificates/Certificate.jsx
+++ src/pages/Certificates/Certificate.jsx
@@ -2,8 +2,8 @@ import styles from './Certificate.module.css'
 export default function Certificate(){
     const certificates = ["VITE_REACT_CERTIFICATE","VITE_SOFTWAREENGINEER_CERTIFICATE","VITE_JAVASCRIPT_BASIC_CERTIFICATE",
                           "VITE_JAVASCRIPT_INTERMEDIATE_CERTIFICATE","VITE_RESTAPI_CERTIFICATE","VITE_SQL_BASIC_CERTIFICATE",
-                        "VITE_SQL_INTERMEDIATE_CERTIFICATE","VITE_JAVA_BASIC_CERTIFICATE","VITE_CRASH_COURSE_PYTHON","VITE_INTRODUCTION_GIT_GITHUB","VITE_TROUBLESHOOTINH_DEBUGGING","VITE_PYTHON_INTERACT_OS"];
-    const tech = ["REACT","Software Engineer","JavaScript","JavaScript Intermediate","REST API","SQL","SQL Intermediate","Java","Python","GitHub","Troubleshooting and Debugging","Python Interacts OS"]
+                        "VITE_SQL_INTERMEDIATE_CERTIFICATE","VITE_JAVA_BASIC_CERTIFICATE","VITE_IT_AUTOMATION_WITH_PYTHON","VITE_AUTOMATING_REAL_WORLD_TASKS_WITH_PYTHON","VITE_CRASH_COURSE_PYTHON","VITE_INTRODUCTION_GIT_GITHUB","VITE_TROUBLESHOOTINH_DEBUGGING","VITE_PYTHON_INTERACT_OS","VITE_LINUX_AND_SQL","VITE_SQL_JOINS","VITE_ART_OF_VISUALIZATION"];
+    const tech = ["REACT","Software Engineer","JavaScript","JavaScript Intermediate","REST API","SQL","SQL Intermediate","Java","IT Automation With Python","Automating Real World Tasks with Python","Python","GitHub","Troubleshooting and Debugging","Python Interacts OS","Linux and SQL","SQL Joins","Share Data Through Art of Visualization"]
     return <>
         <section className={styles.certifications}>
   <h2>Certifications</h2>
